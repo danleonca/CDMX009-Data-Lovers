51f6fb4996f5d7e726f1fe0c5e8854dee5cc8bc1
danleonca <danleon022688@gmail.com>
2020-03-02 22:23:51 -0600

    02mRZO

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import { example } from './data.js';
 
 console.log(example);
 //Llamámos a todos los botones
-document.getElementById("filterByType").addEventListener('click',filterType);
-document.getElementById("filterByWeackness").addEventListener('click',filterWeackness);
-document.getElementByClass("goToTheTop").addEventListener('click',goingBack);
-document.getElementById("findYourCandy").addEventListener('click', numberOfCandys);
+document.getElementById("FilterByType").addEventListener('click',filterType);
+document.getElementById("FilterByWeackness").addEventListener('click',filterWeackness);
+//document.querySelectorAll("goToTheTop").addEventListener('click',goingBack);
+//document.getElementByClassName("goToTheTop").addEventListener('click',goingBack);
+document.getElementById('ButtonSecondPage').addEventListener('click', goToTheTopSP);
+document.getElementById('ButtonThirdPage').addEventListener('click', goToTheToTP);
+document.getElementById('ButtonFourthPage').addEventListener('click', goToTheTopFP);
+//document.getElementById("findYourCandy").addEventListener('click', numberOfCandys);
 //aquí me falta darle funcionalidad a cada uno de los pokemoes al darles click para que muestren su descripción
 //hacemos las funciones de cada botón
 
@@ -23,16 +27,25 @@ function filterWeackness() {
   document.getElementById("FourthPage").style.display= 'none';
 }
 
-function goingBack() {
+function goToTheTopSP(){
+ document.getElementById("FirstPage").style.display= 'block'
+ document.getElementById("SecondPage").style.display= 'none';
+
+}
+
+function goToTheToTP(){
   document.getElementById("FirstPage").style.display= 'block'
-  document.getElementById("SecondPage").style.display= 'none';
+
   document.getElementById("ThirdPage").style.display= 'none';
+
+}
+
+function goToTheTopFP() {
+  document.getElementById("FirstPage").style.display= 'block'
   document.getElementById("FourthPage").style.display= 'none';
 }
 
-function numberOfCandys() {
-  document.getElementById("FirstPage").style.display= 'block'
-  document.getElementById("SecondPage").style.display= 'none';
-  document.getElementById("ThirdPage").style.display= 'none';
-  document.getElementById("FourthPage").style.display= 'none';
-}
+
+//function numberOfCandys() {
+//
+//}
