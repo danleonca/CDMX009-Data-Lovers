ca3beb8969ab38372379b0400fc8a853d886dba1
danleonca <danleon022688@gmail.com>
2020-03-16 22:15:29 -0600

    últimos cambios

--- README.md
+++ README.md
@@ -1,4 +1,102 @@
-# Data Lovers
+# Data Lovers Pokemon Go
+
+## Proceso de Diseño
+
+Nuestro proceso de diseño fue parecido al de cipher, solo que un poco más rápido pues ya teniamos
+algo de conocimiento anterior. Seguimos los mismo pasos para lograr un diseño pensando en
+nuestros usuario pedimos feedback de nuestro prototipo de baja fidelidad y complementamos.
+
+Despues de tener el prototipo de baja fidelidad y tener un poco mas claro lo que queriamos y
+como se veria, visitamos varias paginas relacionadas y notamos que varias de estas no tenian una
+interfaz amigable y eran confusas, asi que tomamos ese descubrimiento como nuestra primera historia de usuario.
+
+Pensamos que nuestra app soluciona los problemas de los maestros/entrenadores pokemon, pues nos muestra una tabla con los 151 pokemons de la region kanto con los elementos que ellos consideran
+importantes (numero pokemon, nombre y una imagen).
+Tambien mostramos por debilidades o tipos, esto les ayuda para conocer las fortalezas de su pokemon y poder estudiar y predecir a sus contrincantes.
+
+Consideramos que los usuarios para poder evolucionar a su pokemon necesitarian saber cuantos dulces se requieren, asi que añadimos un apartado para consultar esa informacion y asi evolucionarlos y avanzar de nivel.
+
+Por ultimo incluye un apartado para poder ver la especificaciones de los pokemons esto para conocer todo sobre su pokemon.
+
+
+![Final](src/images/productofinal.PNG)
+
+![Final](src/images/productofinal1.PNG)
+
+![Final](src/images/productofinal3.PNG)
+
+![Final](src/images/productofinal4.PNG)
+
+
+![Prototipo](src/images/prototipo.PNG)
+
+
+![Prototipo Baja Fidelidad](src/images/bajafidelidad.PNG)
+
+
+
+Liga mockup
+https://www.figma.com/proto/Dg6OYTUK625lLm6F7IlSmT/Pokemon?node-id=0%3A1&scaling=min-zoom
+
+
+## Historias de usuarios
+![historia de usuario](src/images/historias.PNG)
+![historia de usuario](src/images/historias1.PNG)
+
+
+
+
+
+
+
+
+### UX
+
+- [ ✓] Diseñar la aplicación pensando y entendiendo al usuario.
+- [ ✓] Crear prototipos para obtener feedback e iterar.
+- [ ✓] Aplicar los principios de diseño visual (contraste, alineación, jerarquía).
+- [ ] Planear y ejecutar tests de usabilidad.
+
+### HTML y CSS
+
+- [ ✓] Entender y reconocer por qué es importante el HTML semántico.
+- [ ✓] Identificar y entender tipos de selectores en CSS.
+- [ ] Entender como funciona `flexbox` en CSS.
+- [ ✓] Construir tu aplicación respetando el diseño planeado (maquetación).
+
+### DOM
+
+- [✓ ] Entender y reconocer los selectores del DOM (querySelector | querySelectorAll).
+- [ ✓] Manejar eventos del DOM. (addEventListener)
+- [ ✓] Manipular dinámicamente el DOM. (createElement, appendchild, innerHTML, value)
+
+### Javascript
+
+- [ ✓] Manipular arrays (`filter` | `map` | `sort` | `reduce`).
+- [ ✓] Manipular objects (key | value).
+- [ ✓] Entender el uso de condicionales (`if-else` | `switch`).
+- [ ✓] Entender el uso de bucles (`for` | `forEach`).
+- [ ] Entender la diferencia entre expression y statements.
+- [ ✓] Utilizar funciones (parámetros | argumentos | valor de retorno).
+- [ ] Entender la diferencia entre tipos de datos atómicos y estructurados.
+- [ ✓] Utilizar ES Modules (`import` | `export`).
+
+### Pruebas Unitarias (testing)
+- [ ] Testear funciones (funciones puras).
+
+### Git y GitHub
+- [ ✓] Ejecutar comandos de git (`add` | `commit` | `pull` | `status` | `push`).
+- [ ✓] Utilizar los repositorios de GitHub (`clone` | `fork` | gh-pages).
+- [ ✓] Colaborar en Github (pull requests).
+
+### Buenas prácticas de desarrollo
+- [ ✓] Organizar y dividir el código en módulos (Modularización).
+- [ ✓] Utilizar identificadores descriptivos (Nomenclatura | Semántica).
+- [ ✓] Utilizar linter para seguir buenas prácticas (ESLINT).
+
+
+
+*
 
 ## Índice
 
--- test/data.spec.js
+++ test/data.spec.js
@@ -12,3 +12,86 @@
 //   });
 // });
 //
+import {
+  candys, findYourPokemon, filterData, filterWeak, printData, printInfoPokemon,
+}
+  from '../src/data.js';
+
+describe('candys', () => {
+  it('debería ser una función', () => {
+    expect(typeof candys).toBe('function');
+  });
+  it('desbería regresar el atributo candy_acount del arreglo cuando se escribe el nombre de un pokemon', () => {
+    expect(typeof candys()).toBe('object');
+  });
+});
+
+describe('findYourPokemon', () => {
+  it('debaría ser una función', () => {
+    expect(typeof findYourPokemon).toBe('function');
+  });
+  it('debería regresar la info de ese pokemon', () => {
+    expect(typeof findYourPokemon()).toBe('object');
+  });
+});
+
+describe('filterData', () => {
+  it('dería filtrar los pokemones del elemento seleccionado', () => {
+    expect(filterData('Water')).toBeTruthy();
+  });
+  it('debería mostrarme a todos los tipo fuego cuando filtro a los tipo fuego', () => {
+    expect(filterData('Fire')).toEqual([{
+      avg_spawns: 25.3, candy: 'Charmander Candy', candy_count: 25, egg: '2 km', height: '0.61 m', id: 4, img: 'http://www.serebii.net/pokemongo/pokemon/004.png', multipliers: [1.65], name: 'Charmander', next_evolution: [{ name: 'Charmeleon', num: '005' }, { name: 'Charizard', num: '006' }], num: '004', spawn_chance: 0.253, spawn_time: '08:45', type: ['Fire'], weaknesses: ['Water', 'Ground', 'Rock'], weight: '8.5 kg',
+    }, {
+      avg_spawns: 1.2, candy: 'Charmander Candy', candy_count: 100, egg: 'Not in Eggs', height: '1.09 m', id: 5, img: 'http://www.serebii.net/pokemongo/pokemon/005.png', multipliers: [1.79], name: 'Charmeleon', next_evolution: [{ name: 'Charizard', num: '006' }], num: '005', prev_evolution: [{ name: 'Charmander', num: '004' }], spawn_chance: 0.012, spawn_time: '19:00', type: ['Fire'], weaknesses: ['Water', 'Ground', 'Rock'], weight: '19.0 kg',
+    }, {
+      avg_spawns: 0.31, candy: 'Charmander Candy', egg: 'Not in Eggs', height: '1.70 m', id: 6, img: 'http://www.serebii.net/pokemongo/pokemon/006.png', multipliers: null, name: 'Charizard', num: '006', prev_evolution: [{ name: 'Charmander', num: '004' }, { name: 'Charmeleon', num: '005' }], spawn_chance: 0.0031, spawn_time: '13:34', type: ['Fire', 'Flying'], weaknesses: ['Water', 'Electric', 'Rock'], weight: '90.5 kg',
+    }, {
+      avg_spawns: 22, candy: 'Vulpix Candy', candy_count: 50, egg: '5 km', height: '0.61 m', id: 37, img: 'http://www.serebii.net/pokemongo/pokemon/037.png', multipliers: [2.74, 2.81], name: 'Vulpix', next_evolution: [{ name: 'Ninetales', num: '038' }], num: '037', spawn_chance: 0.22, spawn_time: '13:43', type: ['Fire'], weaknesses: ['Water', 'Ground', 'Rock'], weight: '9.9 kg',
+    }, {
+      avg_spawns: 0.77, candy: 'Vulpix Candy', egg: 'Not in Eggs', height: '1.09 m', id: 38, img: 'http://www.serebii.net/pokemongo/pokemon/038.png', multipliers: null, name: 'Ninetales', num: '038', prev_evolution: [{ name: 'Vulpix', num: '037' }], spawn_chance: 0.0077, spawn_time: '01:32', type: ['Fire'], weaknesses: ['Water', 'Ground', 'Rock'], weight: '19.9 kg',
+    }, {
+      avg_spawns: 92, candy: 'Growlithe Candy', candy_count: 50, egg: '5 km', height: '0.71 m', id: 58, img: 'http://www.serebii.net/pokemongo/pokemon/058.png', multipliers: [2.31, 2.36], name: 'Growlithe', next_evolution: [{ name: 'Arcanine', num: '059' }], num: '058', spawn_chance: 0.92, spawn_time: '03:57', type: ['Fire'], weaknesses: ['Water', 'Ground', 'Rock'], weight: '19.0 kg',
+    }, {
+      avg_spawns: 1.7, candy: 'Growlithe Candy', egg: 'Not in Eggs', height: '1.91 m', id: 59, img: 'http://www.serebii.net/pokemongo/pokemon/059.png', multipliers: null, name: 'Arcanine', num: '059', prev_evolution: [{ name: 'Growlithe', num: '058' }], spawn_chance: 0.017, spawn_time: '03:11', type: ['Fire'], weaknesses: ['Water', 'Ground', 'Rock'], weight: '155.0 kg',
+    }, {
+      avg_spawns: 51, candy: 'Ponyta Candy', candy_count: 50, egg: '5 km', height: '0.99 m', id: 77, img: 'http://www.serebii.net/pokemongo/pokemon/077.png', multipliers: [1.48, 1.5], name: 'Ponyta', next_evolution: [{ name: 'Rapidash', num: '078' }], num: '077', spawn_chance: 0.51, spawn_time: '02:50', type: ['Fire'], weaknesses: ['Water', 'Ground', 'Rock'], weight: '30.0 kg',
+    }, {
+      avg_spawns: 1.1, candy: 'Ponyta Candy', egg: 'Not in Eggs', height: '1.70 m', id: 78, img: 'http://www.serebii.net/pokemongo/pokemon/078.png', multipliers: null, name: 'Rapidash', num: '078', prev_evolution: [{ name: 'Ponyta', num: '077' }], spawn_chance: 0.011, spawn_time: '04:00', type: ['Fire'], weaknesses: ['Water', 'Ground', 'Rock'], weight: '95.0 kg',
+    }, {
+      avg_spawns: 10, candy: 'None', egg: '10 km', height: '1.30 m', id: 126, img: 'http://www.serebii.net/pokemongo/pokemon/126.png', multipliers: null, name: 'Magmar', num: '126', spawn_chance: 0.1, spawn_time: '20:36', type: ['Fire'], weaknesses: ['Water', 'Ground', 'Rock'], weight: '44.5 kg',
+    }, {
+      avg_spawns: 1.7, candy: 'Eevee Candy', egg: 'Not in Eggs', height: '0.89 m', id: 136, img: 'http://www.serebii.net/pokemongo/pokemon/136.png', multipliers: null, name: 'Flareon', num: '136', prev_evolution: [{ name: 'Eevee', num: '133' }], spawn_chance: 0.017, spawn_time: '07:02', type: ['Fire'], weaknesses: ['Water', 'Ground', 'Rock'], weight: '25.0 kg',
+    }, {
+      avg_spawns: 0, candy: 'None', egg: 'Not in Eggs', height: '2.01 m', id: 146, img: 'http://www.serebii.net/pokemongo/pokemon/146.png', multipliers: null, name: 'Moltres', num: '146', spawn_chance: 0, spawn_time: 'N/A', type: ['Fire', 'Flying'], weaknesses: ['Water', 'Electric', 'Rock'], weight: '60.0 kg',
+    }]);
+  });
+});
+describe('filterWeak', () => {
+  it('dería filtrar los pokemones y regresar un arreglo nuevo', () => {
+    expect(filterWeak('Water')).toBeTruthy();
+  });
+  it('debería de filtrar a los pokemones que son débiles contra los pokemones tipo venenoso', () => {
+    expect(filterWeak('Poison')).toEqual([{
+      avg_spawns: 236, candy: 'Paras Candy', candy_count: 50, egg: '5 km', height: '0.30 m', id: 46, img: 'http://www.serebii.net/pokemongo/pokemon/046.png', multipliers: [2.02], name: 'Paras', next_evolution: [{ name: 'Parasect', num: '047' }], num: '046', spawn_chance: 2.36, spawn_time: '01:42', type: ['Bug', 'Grass'], weaknesses: ['Fire', 'Ice', 'Poison', 'Flying', 'Bug', 'Rock'], weight: '5.4 kg',
+    }, {
+      avg_spawns: 7.4, candy: 'Paras Candy', egg: 'Not in Eggs', height: '0.99 m', id: 47, img: 'http://www.serebii.net/pokemongo/pokemon/047.png', multipliers: null, name: 'Parasect', num: '047', prev_evolution: [{ name: 'Paras', num: '046' }], spawn_chance: 0.074, spawn_time: '01:22', type: ['Bug', 'Grass'], weaknesses: ['Fire', 'Ice', 'Poison', 'Flying', 'Bug', 'Rock'], weight: '29.5 kg',
+    }, {
+      avg_spawns: 78, candy: 'Exeggcute Candy', candy_count: 50, egg: '5 km', height: '0.41 m', id: 102, img: 'http://www.serebii.net/pokemongo/pokemon/102.png', multipliers: [2.7, 3.18], name: 'Exeggcute', next_evolution: [{ name: 'Exeggutor', num: '103' }], num: '102', spawn_chance: 0.78, spawn_time: '09:09', type: ['Grass', 'Psychic'], weaknesses: ['Fire', 'Ice', 'Poison', 'Flying', 'Bug', 'Ghost', 'Dark'], weight: '2.5 kg',
+    }, {
+      avg_spawns: 1.4, candy: 'Exeggcute Candy', egg: 'Not in Eggs', height: '2.01 m', id: 103, img: 'http://www.serebii.net/pokemongo/pokemon/103.png', multipliers: null, name: 'Exeggutor', num: '103', prev_evolution: [{ name: 'Exeggcute', num: '102' }], spawn_chance: 0.014, spawn_time: '12:34', type: ['Grass', 'Psychic'], weaknesses: ['Fire', 'Ice', 'Poison', 'Flying', 'Bug', 'Ghost', 'Dark'], weight: '120.0 kg',
+    }, {
+      avg_spawns: 22.8, candy: 'None', egg: '5 km', height: '0.99 m', id: 114, img: 'http://www.serebii.net/pokemongo/pokemon/114.png', multipliers: null, name: 'Tangela', num: '114', spawn_chance: 0.228, spawn_time: '23:13', type: ['Grass'], weaknesses: ['Fire', 'Ice', 'Poison', 'Flying', 'Bug'], weight: '35.0 kg',
+    }]);
+  });
+});
+describe('printData', () => {
+  it('debe ser una función', () => {
+    expect(typeof printData).toBe('function');
+  });
+});
+describe('printInfoPokemon', () => {
+  it('debe ser una función', () => {
+    expect(typeof printInfoPokemon).toBe('function');
+  });
+});
